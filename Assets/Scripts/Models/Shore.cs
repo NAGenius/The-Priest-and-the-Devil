4e3eeb1216d029c0ef067ec8ad8c68c4103857c8
及川徹は天才ではない <132603128+NAGenius@users.noreply.github.com>
2024-10-29 22:37:12 +0800

    Initial commit

---
+++ Assets/Scripts/Models/Shore.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shore 
+{
+    public GameObject shore;
+    public int priestCount, devilCount;
+    public Shore (Vector3 position){
+        shore = GameObject.Instantiate(Resources.Load("Prefabs/left_shore", typeof(GameObject))) as GameObject;
+        shore.transform.localScale = new Vector3(10,4.8f,2);
+        shore.transform.position = position;
+        shore.transform.Rotate(0, 180, 0); // 旋转 180 度
+        priestCount = devilCount = 0;
+    }
+}
---
+++ Assets/Scripts/Views/UserGUI.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserGUI : MonoBehaviour
+{
+    IUserAction userAction;
+    public string gameMessage;
+    public int time;
+    private GUIStyle titleStyle, textStyle, timeStyle;
+    private readonly string title = "Priests and Devils";
+    // public Texture2D backgroundImage; // 背景图片
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 60;
+        userAction = SSDirector.GetInstance().CurrentSceneController as IUserAction;
+
+        // backgroundImage = Resources.Load<Texture2D>("Textures/sky");
+        
+        // 设置 Title 的样式
+        titleStyle = new GUIStyle();
+        titleStyle.normal.textColor = Color.white;
+        titleStyle.fontSize = 50;
+        titleStyle.alignment = TextAnchor.MiddleCenter; // 设置文字居中对齐
+
+        textStyle = new GUIStyle();
+        textStyle.normal.textColor = Color.red;
+        textStyle.fontSize = 30;
+        textStyle.alignment = TextAnchor.MiddleCenter;
+
+        timeStyle = new GUIStyle();
+        timeStyle.normal.textColor = new Color(0.529f, 0.808f, 0.922f, 1.0f);
+        timeStyle.fontSize = 30;
+        timeStyle.alignment = TextAnchor.MiddleCenter;
+    }
+
+    // Update is called once per frame
+    void OnGUI()
+    {
+        // if (backgroundImage != null)
+        // {
+        //     // 设置背景图片在屏幕上的大小，使其填满整个屏幕
+        //     GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), backgroundImage, ScaleMode.StretchToFill);
+        // }
+
+        userAction.Check();
+
+        // 在指定位置绘制Label
+        GUI.Label(new Rect((Screen.width - 400f) / 2, 10f, 400f, 50f), title, titleStyle);
+        GUI.Label(new Rect((Screen.width - 400f) / 2, 80f, 400f, 30f), gameMessage, textStyle);
+        GUI.Label(new Rect(10, 0, 100, 50), "Time: " + time, timeStyle);
+    }
+}
